dfe8f7fe7b4f10064e5b712d405998a269e64427
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2019-06-27 13:17:52 +0200

    umm

--- menu.py
+++ menu.py
@@ -8,7 +8,7 @@ import time
 
 # get_terminal_size((0, 0))
 
-#
+# https://docs.python.org/3.7/library/curses.html
 
 class main():
 
@@ -18,11 +18,22 @@ class main():
 
         self.height, self.width = self.window.getmaxyx()
 
-        self.boxes = [(0, 0)]
+        self.boxes = [(0, 0, lambda x: None)]
         self.indexOfBoxes = 0
+        self.buffer = bytearray()
 
-    def addBox(self, y, x):
-        self.boxes.append((y, x))
+    def addBox(self, y, x, f=lambda x: None):
+        self.boxes.append((y, x, f))
+
+    def makeCmdLine(self):
+        def runCmdLine(arg):
+            arg = arg.decode().strip()
+            if arg == 'quit' or arg == 'exit':
+                raise Exception('exiting')
+
+        self.window.addstr(self.height - 2, 0, '-' * self.width)
+        self.window.addstr(self.height - 1, 0, '> ')
+        self.addBox(self.height - 1, 2, runCmdLine)
 
     def captureKey(self):
         a = [self.window.getch()]
@@ -51,23 +62,55 @@ class main():
         return a
 
     def setPosition(self):
-        self.window.move(*self.boxes[self.indexOfBoxes])
+        try:
+            self.window.move(*self.boxes[self.indexOfBoxes][:-1])
+        except IndexError as e:
+            f = open('error.log', 'w')
+            f.write(str(e) + '\n')
+            f.write('self.indexOfBoxes = {:d}\n'.format(self.indexOfBoxes))
+            f.write('self.boxes = {}\n'.format(self.boxes))
+            f.close()
+            raise
 
     def processKey(self):
         k = self.captureKey()
         if type(k) == int:
-            return k
+            y = self.boxes[self.indexOfBoxes][0]
+            x = self.boxes[self.indexOfBoxes][1]
+            if k == 10:
+                self.boxes[self.indexOfBoxes][2](self.buffer)
+                self.buffer = bytearray()
+            if k == 127:
+                if len(self.buffer) > 0:
+                    self.buffer = self.buffer[:-1]
+                    self.window.addstr(y, x + len(self.buffer), ' ')
+            elif k < 256:
+                self.buffer.append(k)
+                self.window.addstr(3, 0, str(x+len(self.buffer)))
+                
+                self.window.refresh()
 
-        if k == 'up' or k == 'left':
+                self.window.addstr(y, x, self.buffer.decode())
+            
+            self.window.addstr(1, 0, str(k))
+            
+                
+
+        if (k == 'up' or k == 'left') and self.indexOfBoxes > 0:
+            self.indexOfBoxes -= 1
+            self.setPosition()
+            self.buffer = bytearray()
+        elif (k == 'down' or k == 'right') and self.indexOfBoxes < len(self.boxes) - 1:
             self.indexOfBoxes += 1
             self.setPosition()
-        elif k == 'down' or k == 'right':
-            pass
+            self.buffer = bytearray()
 
         self.setPosition()        
 
     def run(self):
         self.window.move(0, 0)
+
+        self.makeCmdLine()
 
         self.addBox(10, 0)
 
